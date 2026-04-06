a3559f26ac53859bfc2fdbb8c8c64e49734bbeac
javatechivkp <sreenivasarao.javasoft9@gmail.com>
2026-04-06 15:11:44 +0530

    main changed

--- src/main/java/com/org/java/SpringbootEndToEndAppApplication.java
+++ src/main/java/com/org/java/SpringbootEndToEndAppApplication.java
@@ -2,13 +2,20 @@ package com.org.java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootEndToEndAppApplication {
+public class SpringbootEndToEndAppApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringbootEndToEndAppApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootEndToEndAppApplication.class, args);
-System.out.println("hello");
+
     }
 
 }
